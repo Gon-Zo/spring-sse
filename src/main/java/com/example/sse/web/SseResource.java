36a214f797d8a40e83fff593af981f1f6c0d1e84
gon-zo <pnw123456@icloud.com>
2021-12-28 21:44:23 +0900

    feat: sse connection dev and push event

--- src/main/java/com/example/sse/web/SseResource.java
+++ src/main/java/com/example/sse/web/SseResource.java
@@ -2,14 +2,23 @@ package com.example.sse.web;
 
 import com.example.sse.service.SseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SseResource {
+
+    private static final Map<String, SseEmitter> CLIENTS = new ConcurrentHashMap<>();
 
     private final SseService sseService;
 
@@ -19,8 +28,35 @@ public class SseResource {
     }
 
     @GetMapping(value = "fetch-dataset")
-    public SseEmitter fetchDataSet(){
+    public SseEmitter fetchDataSet() {
         return sseService.fetchDataSet();
+    }
+
+    @GetMapping("/api/subscribe")
+    public SseEmitter subscribe(String id) {
+        SseEmitter emitter = new SseEmitter();
+        CLIENTS.put(id, emitter);
+
+        emitter.onTimeout(() -> CLIENTS.remove(id));
+        emitter.onCompletion(() -> CLIENTS.remove(id));
+        return emitter;
+    }
+
+    @GetMapping("/api/publish")
+    public void publish(String message) {
+        Set<String> deadIds = new HashSet<>();
+
+        CLIENTS.forEach((id, emitter) -> {
+            try {
+                log.info("message :: {}", message);
+                emitter.send(message, MediaType.APPLICATION_JSON);
+            } catch (Exception e) {
+                deadIds.add(id);
+                log.warn("disconnected id : {}", id);
+            }
+        });
+
+        deadIds.forEach(CLIENTS::remove);
     }
 
 }
