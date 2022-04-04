3400423c21bc058d7ecd1d8c8c3b4645435b1b9f
gon-zo <pnw123456@icloud.com>
2022-04-04 20:30:59 +0900

    ResponseEntity 예제

--- spring-mvc/src/main/java/com/example/springweb/web/StartResource.java
+++ spring-mvc/src/main/java/com/example/springweb/web/StartResource.java
@@ -3,6 +3,9 @@ package com.example.springweb.web;
 import com.example.springweb.domain.CurrentDate;
 import com.example.springweb.repository.CurrentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,32 @@ public class StartResource {
   private final CurrentRepository currentRepository;
 
   @GetMapping()
-  public List<CurrentDate> getNowDate() {
-    return currentRepository.findAll();
+  public ResponseEntity<List<CurrentDate>> getNowDate() {
+    List<CurrentDate> body = currentRepository.findAll();
+    return ResponseEntity.ok(body);
+  }
+
+  @GetMapping("/hello")
+  public ResponseEntity<Boolean> getBooleanValue(Boolean isHello) {
+
+    if (isHello) {
+      // isHello 가 ture 로 넘겨지면 HttpStatus 200 반환
+      return ResponseEntity.status(HttpStatus.OK).body(isHello);
+    }
+
+    // isHello 가 false 로 넘겨지면 HttpStatus 204 반환
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(isHello);
+  }
+
+  @GetMapping("/start")
+  public ResponseEntity<String> getCustomHeader() {
+    return ResponseEntity.status(HttpStatus.OK)
+        .header("CUSTOM-HEADER", "this custom")
+        .body("start");
+  }
+
+  @GetMapping("/bad")
+  public ResponseEntity<String> getBadRes() {
+    return ResponseEntity.badRequest().body("bad response");
   }
 }
