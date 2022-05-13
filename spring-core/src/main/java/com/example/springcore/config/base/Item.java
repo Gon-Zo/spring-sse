4539351a71d3ee59c4170ac88e5c258bceb7cedc
gon-zo <pnw123456@icloud.com>
2022-05-13 19:20:12 +0900

    feat: @Lazy 어노테이션 예제 코드

--- spring-core/src/main/java/com/example/springcore/config/base/Item.java
+++ spring-core/src/main/java/com/example/springcore/config/base/Item.java
@@ -1,7 +1,12 @@
 package com.example.springcore.config.base;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+
 public class Item extends AbstractItem{
 
+    @Autowired @Lazy
+    private Item2 item2;
 
     @Override
     void show() {
