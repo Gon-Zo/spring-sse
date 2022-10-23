8cc49e1a3155007b5a25dd04c8f4782e0826fd08
newbalancer <pnw123456@icloud.com>
2022-10-23 12:56:47 +0900

    feat: comment add

--- jpa/src/main/java/com/example/app/domain/user/UserRegistration.java
+++ jpa/src/main/java/com/example/app/domain/user/UserRegistration.java
@@ -3,6 +3,7 @@ package com.example.app.domain.user;
 import com.example.app.constract.RegistrationType;
 import com.example.app.domain.base.BaseTimeEntity;
 import lombok.*;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -19,27 +20,38 @@ public class UserRegistration extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Comment("가입 유형")
     @Enumerated(EnumType.STRING)
     private RegistrationType type;
 
+    @Comment("플랫폼")
     private String platform;
 
+    @Comment("스토어")
     private String store;
 
+    @Comment("나라")
     private String country;
 
+    @Comment("아이피")
     private String ip;
 
+    @Comment("어디서 왔는지 채널명을 기록 쿠키에서 읽음")
     private String channel;
 
+    @Comment("어디서 왔는지 referer url 기록")
     private String referer;
 
+    @Comment("처음 도달한 페이지 path")
     private String landingUrl;
 
+    @Comment("마케팅 이메일 수신 동의 여부")
     private Boolean isAgreedMarketingEmail;
 
+    @Comment("인증 토큰")
     private String verificationToken;
 
+    @Comment("넘어간 시간..?")
     private LocalDateTime enteredDate;
 
     @OneToOne
